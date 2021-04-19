b446f27ae68d9292883f4e56285b84d6c79ddb29
Doug Jones <DSJR1284@GMAIL.COM>
2021-04-19 15:25:24 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,17 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :name, :content
+  
   belongs_to :user
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
 
-  validates_presence_of :name, :content
+  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
+
+  # def tags_attributes=(tag_info)  
+  #   if !tag_info["0"]["name"].blank?
+  #     binding.pry
+  #   end 
+  # end 
+
 end
